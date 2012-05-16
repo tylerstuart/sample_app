c6a88a97d2204b9761b8ff3769c557a2979b4033
Tyler Stuart <tstuart@1on1.com>
2012-05-16 11:49:29 -0600

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+    describe "Home page" do
+    	it "should have the content 'Sample App'" do 
+    		visit '/static_pages/home'
+    		page.should have_content('Smoken Recipes')
+    	end
+
+    	it "should have the right title" do
+    		visit '/static_pages/home'
+    		page.should have_selector('title', :text => "Smoken Recipes | Home")
+    	end
+    end
+
+    describe "Help page" do
+    	it "should have the content 'Help'" do
+    		visit '/static_pages/help'
+    		page.should have_content('Help')
+    	end
+
+    	it "should have the right title" do
+    		visit '/static_pages/help'
+    		page.should have_selector('title', :text => "Smoken Recipes | Help")
+    	end    	
+    end
+
+    describe "About page" do
+    	it "should have the content 'About Us'" do
+    		visit '/static_pages/about'
+    		page.should have_content('About Us')
+    	end
+
+    	it "should have the right title" do
+    		visit '/static_pages/about'
+    		page.should have_selector('title', :text => "Smoken Recipes | About Us")
+    	end
+    end
+end
